2eff712d661c62b5f7b5bb97836078159d9f552e
sourena83 <113782009+sourena83@users.noreply.github.com>
2022-10-02 04:15:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -73,13 +73,22 @@ prettify.toHTML(run(), console);
 
 
 
-var post= document.getElementById("post");
-post.addEventListener("click", function(){
-    var commentBoxValue= document.getElementById("comment-box").value;
- 
-    var li = document.createElement("li");
-    var text = document.createTextNode(commentBoxValue);
-    li.appendChild(text);
-    document.getElementById("unordered").appendChild(li);
- 
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
+
+
+
+document.getElementById('button').addEventListener('click', (event) => {
+  let element_outputName = document.getElementById('outputName');
+  element_outputName.innerText = getNumberOrString(document.getElementById('inputName').value);
+  let element_outputText = document.getElementById('outputText');
+  element_outputText.innerText = getNumberOrString(document.getElementById('inputComment').value);
+
 });
